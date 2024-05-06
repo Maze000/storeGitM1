c73777b02e31673a5b89be989397c3d71ca29b96
MAZE <72741681+Maze000@users.noreply.github.com>
2024-05-07 01:12:24 +1200

    Update Goo.js

--- src/components/Goo.js
+++ src/components/Goo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './css/goo.css';
 const GoogleLoginButton = () => {
   const handleLogin = () => {
-    console.log('aqui se esta ejecutando el boton de google');
+    
     window.location.href = '/auth/google';
 
   };
